efe75234de41107c473328631e7110db9f118a39
amakil76 <38749918+amakil76@users.noreply.github.com>
2025-02-19 23:10:16 +0800

    Delete example3.py

--- example3.py
+++
@@ -1,6 +0,0 @@
-#Pilih beberapa item secara rawak
-import random
-
-warna = ["merah", "biru", "hijau", "kuning", "hitam"]
-pilihan = random.sample(warna, 2)  # Pilih 2 item tanpa pengulangan
-print("Warna yang dipilih:", pilihan)
