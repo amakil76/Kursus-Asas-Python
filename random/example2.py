22aec34f1d717e54af11c5dd41a02fdffb8e45d9
amakil76 <38749918+amakil76@users.noreply.github.com>
2025-02-19 23:07:59 +0800

    Pilih item secara rawak dari senarai

---
+++ random/example2.py
@@ -0,0 +1,7 @@
+#Pilih item secara rawak dari senarai
+
+import random
+
+buah = ["epal", "pisang", "mangga", "anggur"]
+pilihan = random.choice(buah)  # Pilih satu secara rawak
+print("Buah yang dipilih:", pilihan)
